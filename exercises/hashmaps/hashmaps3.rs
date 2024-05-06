32e9b43bd4125821338a428e3aa0356073598211
JackyLee-mac <leejiayu333@gmail.com>
2024-05-06 17:41:43 +0800

    hashmaps options quiz2 done!

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team_1 = scores
+            .entry(team_1_name.clone())
+            .or_insert(Team {
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+        (*team_1).goals_scored += team_1_score;
+        (*team_1).goals_conceded += team_2_score;
+
+        let team_2 = scores
+            .entry(team_2_name.clone())
+            .or_insert(Team {
+                goals_scored: 0,
+                goals_conceded: 0,
+            });
+
+        (*team_2).goals_scored += team_2_score;
+        (*team_2).goals_conceded += team_1_score;
     }
     scores
 }
@@ -82,5 +99,12 @@ mod tests {
         let team = scores.get("Spain").unwrap();
         assert_eq!(team.goals_scored, 0);
         assert_eq!(team.goals_conceded, 2);
+    }
+    #[test]
+    fn validate_team_score_3() {
+        let scores = build_scores_table(get_results());
+        let team = scores.get("France").unwrap();
+        assert_eq!(team.goals_scored, 5);
+        assert_eq!(team.goals_conceded, 5);
     }
 }
