43dcfed1cc54f13da0806b3645d4035d66f6617b
JackyLee-mac <leejiayu333@gmail.com>
2024-05-07 18:00:47 +0800

    options2-3 error_handling1-5 done!

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -32,8 +30,8 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-         while let Some(Some(integer)) = optional_integers.pop() {
-           assert_eq!(integer, cursor);
+        while let Some(Some(integer)) = optional_integers.pop() {
+            assert_eq!(integer, cursor);
             cursor -= 1;
         }
 
